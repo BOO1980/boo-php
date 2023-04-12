b6c6faa04e0456db6659165878673aa0691d4e69
Hayley <hbillingham80@Gmail.com>
2023-04-12 17:33:30 +0100

    php: while loops

---
+++ work/php-handbook/13-while-loop/index.php
@@ -0,0 +1,13 @@
+<?php 
+    // while(true){
+    //     echo 'looping';
+    // }
+//this would be an infinite
+$counter = 0;
+while($counter <10){
+    echo "$counter\n";
+    $counter++;
+}
+
+
+?>
